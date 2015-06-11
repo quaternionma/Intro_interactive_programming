a7948357c7da0dcf05e6ca47dd311869d360befb
quaternionma <marco.savoca76@gmail.com>
2015-06-11 13:18:35 +0200

    Implemented step 1 of extra functionality

--- I/guess_a_number/step2.guess_a_number.py
+++ I/guess_a_number/step2.guess_a_number.py
@@ -6,27 +6,33 @@ import random
 import simplegui
 import math
 
-# helper function to start and restart the game
-def new_game():
-    # initialize global variables used in your code here
-    global secret_number
-    secret_number = random.randrange(0,100)
-    
-    frame.start()
+# initialize global variables used in your code here
+upper_limit = 100
+secret_number= 0 
 
+# helper function to start and restart the game
+def new_game():        
+    global secret_number    
+    secret_number = random.randrange(0,upper_limit)
+    
+    print "Game (re)-starts in the range: 0-" + str(upper_limit)
+    
 
 
 # define event handlers for control panel
 def range100():
     # button that changes the range to [0,100) and starts a new game 
+    global upper_limit
+    upper_limit = 100
+    new_game()
     
-    # remove this when you add your code    
-    pass
 
 def range1000():
     # button that changes the range to [0,1000) and starts a new game     
+    global upper_limit
+    upper_limit = 1000
+    new_game()
     
-    pass
     
 def input_guess(guess):
     # main game logic goes here	
@@ -34,21 +40,22 @@ def input_guess(guess):
     guess = int(guess)
     print "secret", secret_number
     if secret_number < guess:
-        print "Lower"     
+        print "Lower\n"     
     if secret_number > guess:
-        print "Higher"
+        print "Higher\n"
     if secret_number == guess:
-        print "Correct!"
+        print "Correct!\n"
      
     
 # create frame
 frame = simplegui.create_frame("Guess the Number", 300, 300)
 
 # register event handlers for control elements and start frame
-frame.add_input("Your Guess Please", input_guess, 100)
-
+frame.add_input("Your Guess Please", input_guess, 200)
+frame.add_button("Range: [0,100)", range100, 200)
+frame.add_button("Range: [0,1000)", range1000, 200)
 # call new_game 
 new_game()
-
+frame.start()
 
 # always remember to check your completed program against the grading rubric
