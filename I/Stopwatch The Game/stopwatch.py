08ddaf06a2d3deb1710d24551d494cf2d4bf6749
quaternionma <marco.savoca76@gmail.com>
2015-06-18 23:43:44 +0200

    Stopwatch - Step 3, added buttons start, stop, reset - change font size to 32

--- I/Stopwatch The Game/stopwatch.py
+++ I/Stopwatch The Game/stopwatch.py
@@ -9,8 +9,16 @@ def format(t):
     pass
     
 # define event handlers for buttons; "Start", "Stop", "Reset"
-
-
+def start():
+    timer.start()
+    
+def stop():
+    timer.stop()
+    
+def reset():
+    global time
+    time = 0
+    
 # define event handler for timer with 0.1 sec interval
 def tick():
     global time
@@ -21,7 +29,7 @@ def tick():
 def draw_time(canvas):
     global time
     time_canvas = str(time)
-    canvas.draw_text(time_canvas, (140, 110), 24, 'White')
+    canvas.draw_text(time_canvas, (140, 110), 32, 'White')
     
     
 # create frame
@@ -29,7 +37,9 @@ frame = simplegui.create_frame("Stopwatch: The Game", 300, 200)
 
 # register event handlers
 frame.set_draw_handler(draw_time)
-
+button_start = frame.add_button('START', start, 100)
+button_stop = frame.add_button('STOP', stop, 100)
+button_reset = frame.add_button('RESET', reset, 100)
 # create a timer with 100 ms interval
 timer = simplegui.create_timer(100, tick)
 
