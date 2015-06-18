2d6894e13f978e0a4f9443de6a9fa0d90559f072
quaternionma <marco.savoca76@gmail.com>
2015-06-19 00:13:23 +0200

    Stopwatch - Step 3, added some space between buttons and centered text on canvas

--- I/Stopwatch The Game/stopwatch.py
+++ I/Stopwatch The Game/stopwatch.py
@@ -2,6 +2,8 @@
 import simplegui
 # define global variables
 time = 0
+canvas_width = 200
+canvas_height = 150
 
 # define helper function format that converts time
 # in tenths of seconds into formatted string A:BC.D
@@ -29,22 +31,30 @@ def tick():
 def draw_time(canvas):
     global time
     time_canvas = str(time)
-    canvas.draw_text(time_canvas, (140, 110), 32, 'White')
+    
+    # gets the textwidth drawn on the canvas in order to center the text
+    textwidth = frame.get_canvas_textwidth(time_canvas, 32, 'sans-serif')
+    text_center = textwidth / 2
+    
+    # draws the text on canvas
+    canvas.draw_text(time_canvas, ((canvas_width/2) - text_center, 90), 32, 'White', 'sans-serif')
     
     
 # create frame
-frame = simplegui.create_frame("Stopwatch: The Game", 300, 200)
+frame = simplegui.create_frame("Stopwatch: The Game", canvas_width, canvas_height)
 
 # register event handlers
 frame.set_draw_handler(draw_time)
 button_start = frame.add_button('START', start, 100)
+frame.add_label( "" )
 button_stop = frame.add_button('STOP', stop, 100)
+frame.add_label( "" )
 button_reset = frame.add_button('RESET', reset, 100)
 # create a timer with 100 ms interval
 timer = simplegui.create_timer(100, tick)
 
 # start frame
 frame.start()
-timer.start()
+#timer.start()
 
 # Please remember to review the grading rubric
