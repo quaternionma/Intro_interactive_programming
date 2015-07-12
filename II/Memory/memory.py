e322c7f8b8e77dabc1d6bf4c1354df2aab79d5a0
quaternionma <marco.savoca76@gmail.com>
2015-07-13 00:32:55 +0200

    Step 7-8 implemented

--- II/Memory/memory.py
+++ II/Memory/memory.py
@@ -9,19 +9,21 @@ y_card = CARD_HEIGTH / 2
 NUMBER_OF_CARDS = 16
 canvas_w = CARD_WIDTH * NUMBER_OF_CARDS
 canvas_h = CARD_HEIGTH
+EXPOSED = True
+NOT_EXPOSED = False
 
 # helper function to initialize globals
 def new_game():
-    global deck, exposed
-    
+    global deck, exposed, state, clicked_idx_store
+    state = 0
+    clicked_idx_store = [None, None]
     deck_1 = range(8)
     deck_2 = range(8)
     deck_1.extend(deck_2)
     deck = deck_1
-    exposed = [True,False] * 8
+    exposed = [NOT_EXPOSED] * 16
     random.shuffle(deck)
-
-     
+    
 # define event handlers
 def mouseclick(pos):
     # add game state logic here
@@ -29,12 +31,23 @@ def mouseclick(pos):
         return ((pos[0] >= (card - x_card)) and (pos[0] < (card + x_card)))
     clicked = filter(is_clicked, x_pos)
     clicked_idx = x_pos.index(clicked[0])
-    print clicked_idx
-    
+
+    global state, clicked_idx_store
     if not exposed[clicked_idx]:
-        exposed[clicked_idx] = True
-    
-                        
+        exposed[clicked_idx] = EXPOSED
+        if state == 0:
+            state = 1
+            clicked_idx_store[0] = clicked_idx
+        elif state == 1:
+            state = 2
+            clicked_idx_store[1] = clicked_idx        
+        else:
+            state = 1     
+            if deck[clicked_idx_store[0]] != deck[clicked_idx_store[1]]:
+                exposed[clicked_idx_store[0]] = NOT_EXPOSED
+                exposed[clicked_idx_store[1]] = NOT_EXPOSED
+            clicked_idx_store[0] = clicked_idx
+
 # cards are logically 50x100 pixels in size    
 def draw(canvas):
     global x_pos
@@ -51,7 +64,7 @@ def draw(canvas):
         if  not card_stat:
             left = x_pos[ind_exp] - x_card
             right = x_pos[ind_exp] + x_card
-            canvas.draw_polygon([[left, 0], [right, 0], [right, 100], [left, 100]], 3, 'Green', 'Green')
+            canvas.draw_polygon([[left, 0], [right, 0], [right, 100], [left, 100]], 2, 'Gray', 'Green')
         ind_exp += 1
 
 # create frame and add a button and labels
